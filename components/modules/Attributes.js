9a9ffe532a38c568cd3c10658fc7d1a804d1a8b4
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-02-17 22:27:42 +0900

    home page

---
+++ components/modules/Attributes.js
@@ -0,0 +1,34 @@
+import styles from './Atributes.module.css';
+
+import Choice from "../icons/Choice";
+import Clock from "../icons/Clock";
+import Fast from "../icons/Fast";
+import Food from "../icons/Food";
+
+function Attributes() {
+  return (
+    <div className={styles.container}>
+      <h3>Why us?</h3>
+      <div className={styles.subContainer}>
+        <div>
+          <Fast />
+          <p>Fast</p>
+        </div>
+        <div>
+          <Food />
+          <p>Best Restaurants</p>
+        </div>
+        <div>
+          <Choice />
+          <p>Your Choice</p>
+        </div>
+        <div>
+          <Clock />
+          <p>24-7</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Attributes;
--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Attributes from '../modules/Attributes';
 import Banner from '../modules/Banner';
+import Definition from '../modules/Definition';
 import styles from './HomePage.module.css'
 
 const HomePage = () => {
     return (
         <div className={styles.container}>
-            <Banner/> 
+            <Banner /> 
+            <Attributes />
+            <Definition/>
         </div>
     );
 };

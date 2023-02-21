94ba48d860e44e76e5464bb33f463b8e63d43280
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-02-21 20:24:49 +0900

    path small change

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -10,13 +10,13 @@ export async function getStaticPaths() {
   const res = await fetch("https://my-server-pi.vercel.app/food-data");
   const jsonData = await res.json();
   const data = jsonData.slice(0, 10);
-  const path = data.map((food) => {
+  const path = data.map((food) => ({
     params: {
-      id: food.id.toString();
+      id: food.id.toString()
     }
-  });
+  }));
     return {
         path,
-        fallback:true
+        fallback:true,
     }
 }
